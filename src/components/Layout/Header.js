cb42dabb3a06596074cb4720da87d3e7383c2e51
Surya chitturi <teju.chitturi@hotmail.com>
2021-05-14 11:26:00 +0200

    basic structure

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import {Fragment} from 'react';
+import HeaderCartButton from './HeaderCartButton';
+import image from '../../assets/image.jpg';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return (
+    <Fragment>
+        <header className={classes.header}>
+            <h1>Food Ordering App</h1>
+           <HeaderCartButton/>
+            </header>
+        <div className={classes['main-image']}>
+            <img src={image} alt="img"></img>
+        </div>
+    </Fragment>
+    );
+};
+
+export default Header;
